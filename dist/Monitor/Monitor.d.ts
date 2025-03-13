0ee777ccb6a9f59a4ba922467c4f0b160f78ebca
Hadi Lee <leeshuzheng@gmail.com>
2025-03-13 17:18:28 +0800

    chore: add dist

---
+++ dist/Monitor/Monitor.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+declare type MonitorProps = {
+    backgroundStyles?: React.CSSProperties;
+    children?: React.ReactNode;
+    width?: string;
+    height?: string;
+};
+declare const Monitor: React.ForwardRefExoticComponent<MonitorProps & React.RefAttributes<HTMLDivElement>>;
+export { Monitor, MonitorProps };
+//# sourceMappingURL=Monitor.d.ts.map
